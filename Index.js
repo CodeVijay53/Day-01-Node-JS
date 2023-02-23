692efeacfdc3a1104fd8fe9dd54ee826c086406d
Vijay Shankar <vijayoffcl@gmail.com>
2023-02-23 11:40:44 +0530

    added

---
+++ Index.js
@@ -0,0 +1,41 @@
+// Creating a File
+const http = require("http");
+const fs = require("fs");
+const { json } = require("stream/consumers");
+
+// Storing Time in Variable
+const currentTime = new Date();
+const date = currentTime.getDate();
+const month = currentTime.getMonth() + 1;
+const year = currentTime.getFullYear();
+const hour = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const seconds = currentTime.getSeconds();
+const FullDate = `${date}|${month}|${year}`;
+const FullTime = `${hour}hrs|${minutes}mins|${seconds}secs`;
+
+//Create file and write a file
+var writeStream = fs.createWriteStream("current-date-time.txt");
+writeStream.write(`Current Date and Time: ${FullDate} & ${FullTime}`);
+writeStream.end();
+
+//Read File in particular directory
+fs.readdir("./", "utf-8", (err, files) => {
+  if (err) console.log(err);
+  else {
+    console.log(files);
+  }
+});
+
+//API Endpoints
+//create a server object:
+const server = http
+  .createServer((req, res) => {
+    if (req.url === "./") {
+      res.write(
+        JSON.stringify(`Current-Time-Date : ${FullDate} & ${FullTime}`)
+      );
+    }
+    res.end();
+  })
+  .listen(8080);
